887d2e22a57279b74080c8a9bc19748498cbe253
stetson <stetson2001@msn.com>
2023-08-26 00:49:40 -0700

    Components folder and image displayed.

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
 
 export default function App() {
+
+  const placeholderImage = require('./assets/images/background-image.png')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+     <ImageViewer placeholderImage={placeholderImage} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });

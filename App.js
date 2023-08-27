9ef8024ad85adf41fb58075873e71482148f7453
stetson <stetson2001@msn.com>
2023-08-27 16:04:39 -0700

    button icons and open modal button

--- App.js
+++ App.js
@@ -1,39 +1,75 @@
-import {useState} from 'react'
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from "expo-image-picker";
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
+import CircleButton from "./components/CircleButton";
+import IconButton from "./components/IconButton";
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [isAppOptionsShown, setIsAppOptionsShown] = useState(false);
   const placeholderImage = require("./assets/images/background-image.png");
 
   //Function to handle image picking
 
-    const pickImageAsync = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        allowsEditing: true,
-        quality: 1,
-      });
-  
-      if (!result.canceled) {
-       setSelectedImage(result.assets[0].uri)
-      } else {
-        alert('You did not select any image.');
-      }
-    };
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
 
-  
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setIsAppOptionsShown(true);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
+  const onReset = () => {
+    setIsAppOptionsShown(false)
+  };
+
+  const onAddSticker = () => {};
+
+  const onSaveImageAsync = () => {};
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImage={placeholderImage} selectedImage={selectedImage} />
+        <ImageViewer
+          placeholderImage={placeholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" pickImageAsync={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
+      {isAppOptionsShown ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            label="Choose a photo"
+            theme="primary"
+            pickImageAsync={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setIsAppOptionsShown(true)}
+          />
+        </View>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -56,6 +92,14 @@ const styles = StyleSheet.create({
   },
   footerContainer: {
     flex: 1 / 3,
-    alignItems: 'center'
-  }
+    alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
 });
--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,7 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Button({ label, theme, pickImageAsync }) {
+export default function Button({ label, theme, pickImageAsync, onPress }) {
     if(theme === "primary") {
         return (
           <View  style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
@@ -20,7 +20,7 @@ export default function Button({ label, theme, pickImageAsync }) {
 
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+            <Pressable style={styles.button} onPress={onPress}>
               <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
           </View>
